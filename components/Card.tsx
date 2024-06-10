d98dfc0eb170b1e40bb3fd3fa76addc4d266149a
Richard Turney <richard.turney@bytron.aero>
2024-06-10 16:51:05 +0100

    Extract SubTextContainer from card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,12 +2,11 @@ import React, {FC} from 'react';
 import { Text, Image, View } from 'react-native';
 import { pathwayData } from '@/interfaces/pathwayData';
 import { styled } from 'styled-components';
-import Svg, {Circle} from 'react-native-svg';
+import { SubTextContainer } from './SubTextContainer';
 
 type cardProps = {
   data: pathwayData
 }
-
 
 const PathwayImage = styled(Image)`
   height: 200px;
@@ -29,59 +28,12 @@ const TitleText = styled(Text)`
   align-self: left;
   padding: 10px;
 `
-
-const SubContainer = styled(View)`
-  padding: 5px;
-  flex-direction: row;
-  align-self: left;
-`
-const Icon = styled(Image)`
-  height: 20px;
-  width: 20px;
-`
-
-const IconContainer = styled(View)`
-  padding-right: 5px; 
-`
-
-const TypeContainer = styled(View)`
-  padding: 3px;
-  justify-content: center;
-`
-
-const DotIcon = styled(Image)`
-  height: 5px;
-  width: 5px;
-`
-
-const DotIconContainer = styled(View)`
-  padding: 3px;
-  justify-content: center
-`
-
-const DurationContainer = styled(View)`
-  justify-content: center;
-`
-
 export const Card: FC<cardProps> = ({data}) => {
   
   return (
     <CardContainer>
       <PathwayImage source={data.image ? {uri: data.image} : undefined}/>
-      <SubContainer>
-        <IconContainer>
-          <Icon source={require('../assets/icons8-book-50.png')}/>
-        </IconContainer>
-        <TypeContainer>
-          <Text>{data.type}</Text>
-        </TypeContainer>
-        <DotIconContainer>
-          <DotIcon source={require('../assets/circle-grey.png')}/>
-        </DotIconContainer>
-        <DurationContainer>
-          <Text>{data.duration}</Text>
-        </DurationContainer>
-      </SubContainer>
+      <SubTextContainer type={data.type} duration={data.duration}/>
       <TitleText>{data.title}</TitleText>
       <Text>{data.intro}</Text>
       <Text>url: {data.url}</Text>
---
+++ components/SubTextContainer.tsx
@@ -0,0 +1,61 @@
+import React, {FC} from 'react';
+import { View, Text, Image } from 'react-native';
+import { styled } from 'styled-components';
+
+type subTextProps = {
+  type: string,
+  duration: string
+}
+
+const SubContainer = styled(View)`
+  padding: 5px;
+  flex-direction: row;
+  align-self: left;
+`
+const Icon = styled(Image)`
+  height: 20px;
+  width: 20px;
+`
+
+const IconContainer = styled(View)`
+  padding-right: 5px; 
+`
+
+const TypeContainer = styled(View)`
+  padding: 3px;
+  justify-content: center;
+`
+
+const DotIcon = styled(Image)`
+  height: 5px;
+  width: 5px;
+`
+
+const DotIconContainer = styled(View)`
+  padding: 3px;
+  justify-content: center
+`
+
+const DurationContainer = styled(View)`
+  justify-content: center;
+`
+
+export const SubTextContainer: FC<subTextProps> = ({type, duration}) => {
+
+  return (
+    <SubContainer>
+    <IconContainer>
+      <Icon source={require('../assets/icons8-book-50.png')}/>
+    </IconContainer>
+    <TypeContainer>
+      <Text>{type}</Text>
+    </TypeContainer>
+    <DotIconContainer>
+      <DotIcon source={require('../assets/circle-grey.png')}/>
+    </DotIconContainer>
+    <DurationContainer>
+      <Text>{duration}</Text>
+    </DurationContainer>
+  </SubContainer>
+  )
+}
